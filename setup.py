310387fd06a90a50e2db93693cfda7e8993e3458
Ezel <lfepy@gmail.com>
2024-12-30 14:19:28 +0200

    Initialize GPU library repository

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='lfepy-gpu',
+    name='lfepy',
     version='1.0.0',
     author='Dr. Prof. Khalid M. Hosny, MSc. Mahmoud A. Mohamed, Dr. Essa E. Almazroei',
     author_email='lfepy@gmail.com',
     description='lfepy is a Python package for local feature extraction.',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
-    url='https://github.com/lfepy/lfepy',  # Replace with your repository URL
+    url='https://github.com/lfepy/lfepy-gpu',  # Replace with your repository URL
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
