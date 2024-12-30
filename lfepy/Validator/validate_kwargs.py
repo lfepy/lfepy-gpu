d46ee5b8dd7d47fe75310cd6a3d6c8fc3ec10bd5
lfepy <lfepython@gmail.com>
2024-12-30 13:25:20 +0200

    Delete lfepy directory

--- lfepy/Validator/validate_kwargs.py
+++
@@ -1,8 +0,0 @@
-def validate_kwargs(**kwargs):
-    # Handle keyword arguments
-    if kwargs is None:
-        options = {}
-    else:
-        options = kwargs
-
-    return options
--- lfepy/__init__.py
+++
@@ -1,7 +0,0 @@
-# __init__.py in the main package
-__version__ = '1.0.0'
-__author__ = ["Dr. Prof. Khalid M. Hosny", "MSc. Mahmoud A. Mohamed", "Dr. Essa E. Almazroei"]
-__license__ = 'MIT'
-
-# Example of a package-wide configuration
-DEBUG = False
